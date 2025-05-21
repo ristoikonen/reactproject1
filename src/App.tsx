6c17b766fc4122992b75f049bfa5e3a64b328e31
Risto Ikonen <ristoikonen@gmail.com>
2025-05-21 10:49:34 +1000

    Add react-fast-marquee's ticket scroller

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,10 @@ function App() {
   return (
     <>
     
-      
+
         <TickerCard/>
-      
-{/*       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+
+
     </>
   )
 }
--- src/components/TickerCard/TickerCard.tsx
+++ src/components/TickerCard/TickerCard.tsx
@@ -7,18 +7,9 @@
  */
 
 
+import React, { useEffect, useState } from 'react';
+import Marquee from "react-fast-marquee";
 
-
-import React, { useEffect, useState,FC } from 'react';
-
-/* import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
- */
-//import { Card, CardContent } from '~/components/ui/card';
-//import { Loader2, AlertTriangle } from 'lucide-react'; @/components/ui
-//import { TickerCardWrapper } from './TickerCard.styled';
 
 interface Ticker {
     id: string;
@@ -35,61 +26,48 @@ interface TickerCardProps {
 }
 
 const TickerCard: React.FC<TickerCardProps> = ({ ticker }) => {
-   
    const imgSrc = `/src/assets/${ticker.symbol}.png`;
+   const imgSize = 35;
 
    return (
 
+            <div className="card-body justify-content-between media d-flex">
 
-
-        
-            <div className="card-body">
-              <div className="media d-flex">
-                <div className="align-self-center">
-			<h3>{ticker.name}</h3><img src={imgSrc} alt={ticker.symbol} className="img-fluid" height={35} width={35} />
+                <div className="text-start">
+			        <h3>{ticker.name}</h3><img src={imgSrc} alt={ticker.symbol} className="img-fluid" height={imgSize} width={imgSize} />
                 </div>
 
-
-
-                <div className="media-body text-right">
-			<h2>${parseFloat(ticker.price_usd).toLocaleString()}</h2>
-			
-			   <h5>
-
-            <span style={{ color: parseFloat(ticker.percent_change_24h) >= 0 ? '#52c41a' : '#f5222d' }}>
-            {parseFloat(ticker.percent_change_24h)}% 
-            {/* {parseFloat(ticker.percent_change_24h) >= 0 ? '\u2005 \u2191' : '\u2005 \u2193' } */}
-            </span>
-            </h5>
-
-
+                <div className="text-middle">
+                    <span style={{ display: "inline-block", width: "20px" }}></span>
                 </div>
 
-
-              </div>
+                <div className="text-right">
+                    <h2>${parseFloat(ticker.price_usd).toLocaleString()}</h2>
+                    <h5>
+                        <span style={{ color: parseFloat(ticker.percent_change_24h) >= 0 ? '#52c41a' : '#f5222d' }}>
+                        {parseFloat(ticker.percent_change_24h)}% 
+                        {/* {parseFloat(ticker.percent_change_24h) >= 0 ? '\u2005 \u2191' : '\u2005 \u2193' } */}
+                        </span>
+                    </h5>
+                </div>
 
             </div>
-
-
-          
-
-
-
 )};
 
 const CryptoTicker = () => {
     const [tickers, setTickers] = useState<Ticker[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const showCoins = 6;
 
     useEffect(() => {
         const fetchTickers = async () => {
             try {
                 const response = await fetch('https://api.coinlore.net/api/tickers/');
                 const data = await response.json();
-                setTickers(data.data.slice(0, 20)); // Limiting to 20 results for simplicity
+                setTickers(data.data.slice(0, showCoins)); // Limiting to showCoins
             } catch (err) {
-                setError('Failed to fetch data.');
+                setError('Failed to fetch Coinlore data.');
             } finally {
                 setLoading(false);
             }
@@ -105,46 +83,17 @@ const CryptoTicker = () => {
       {error}</div>;
 
     return (
-
-
-
-
-             <div className="grey-bg container-fluid">
-
-      <div className="row">
-               
-
-          {tickers.map(ticker => (
-
-
-<div className="col-4 mt-6 mb-1 col-xl-4 col-sm-5 ">
-   {/* col-6 mt-3 mb-1    col-xl-4 col-sm-5 col-6 */}
-<div className="card">
-     <div className="card-content">
-
-                <TickerCard key={ticker.id} ticker={ticker} />
-                </div>
-</div>
-           
-       </div>
-
-  
-
- 
- )
- 
-  
- 
- )}
-
-
-     </div>
-
-  </div> 
-
-
-
-
+        <div className="row">
+            <Marquee>
+                {tickers.map(ticker => (
+                    <div className="card ms-2" key={ticker.id}>
+                        <div className="card-content">
+                            <TickerCard key={ticker.id} ticker={ticker} />
+                        </div>
+                    </div>
+                ))}
+            </Marquee>
+        </div>
     );
 };
 
